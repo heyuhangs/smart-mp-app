65c989af692721c7bb710fa47b9df56100b49ec0
heyuhang <yuhang.he@zhaopin.com.cn>
2023-09-23 15:21:41 +0800

    feat: 样式优化

--- src/pages/mine/mine.vue
+++ src/pages/mine/mine.vue
@@ -3,16 +3,14 @@
     <view class="mine__top">
       <image class="mine__top__bg" src="@/static/mine/bg.png" />
       <view class="mine__top__user">
-        <image class="mine__top__img" :src="`${env.imgUrl}${obj.avatar}`"
-          lazy-load="true" />
+        <image class="mine__top__img" :src="avatar" lazy-load="true" />
         <view>
-          <view class="mine__top__name">{{obj.nickName}}({{ obj.majorName }})</view>
+          <view class="mine__top__name">{{ obj.nickName }}({{ obj.majorName }})</view>
           <view class="mine__top__address"> {{ obj.schoolName }} </view>
         </view>
       </view>
     </view>
     <view class="mine__menu">
-
       <view class="mine__menu__item" @click="toCurriculum()">
         <view class="mine__menu__left">
           <image class="mine__menu__icon" src="@/static/mine/curriculum.png" />
@@ -28,105 +26,121 @@
         </view>
         <image class="mine__menu__back" src="@/static/basic/back.png" />
       </view>
-
     </view>
   </view>
 </template>
 
 <script setup>
-	import { ref } from 'vue'
-	import { getUser } from '@/store/token'
-	import env from '@/host'
-	
-	const obj = ref(getUser())
-  function toBiographicalNotesList() {
-    uni.navigateTo({url: '/pages/mine/biographicalNotesList'})
+import { computed, ref } from 'vue'
+import { getUser } from '@/store/token'
+import env from '@/host'
+
+import avatarFemaleImage from '@/static/mine/avatar-female.png'
+import avatarMaleImage from '@/static/mine/avatar-male.png'
+
+const obj = ref(getUser())
+
+const avatar = computed(() => {
+  const { avatar, sex } = obj.value
+  if (avatar) {
+    return `${env.imgUrl}${obj.avatar}`
   }
-  function toCurriculum() {
-    uni.navigateTo({url: '/pages/mine/curriculum'})
+  if (`${sex}` === `0`) {
+    return avatarMaleImage
   }
+  return avatarFemaleImage
+})
+
+function toBiographicalNotesList() {
+  uni.navigateTo({ url: '/pages/mine/biographicalNotesList' })
+}
+function toCurriculum() {
+  uni.navigateTo({ url: '/pages/mine/curriculum' })
+}
 </script>
 
 <style lang="scss" scoped>
-  .mine {
-    background-color: #f2f4f5;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    position: fixed;
-    overflow-y: scroll;
-    overflow-x: hidden;
+.mine {
+  background-color: #f2f4f5;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  position: fixed;
+  overflow-y: scroll;
+  overflow-x: hidden;
 
-    &__top {
-      position: relative;
-      &__bg {
-        width: 100%;
-      }
-
-      &__user{
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-        position: absolute;
-        bottom: 50rpx;
-        margin-left: 32rpx;
-      }
-
-      &__img {
-        border-radius: 50% 50%;
-        width: 178rpx;
-        height: 178rpx;
-      }
-
-      &__name {
-        color: #ffffff;
-        font-size: 36rpx;
-        margin-left: 42rpx;
-      }
-
-      &__address {
-        color: #ffffff;
-        font-size: 24rpx;
-        margin-top: 31rpx;
-        margin-left: 42rpx;
-      }
+  &__top {
+    position: relative;
+    &__bg {
+      width: 100%;
+      height: 215px;
     }
 
-    &__menu {
-      background-color: #ffffff;
-      margin-top: 42rpx;
+    &__user {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      position: absolute;
+      bottom: 50rpx;
+      margin-left: 32rpx;
+    }
 
-      &__item {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin: 0 30rpx;
-        padding: 40rpx 0;
-        border-bottom: 1rpx solid #F2F2F2;
-      }
+    &__img {
+      border-radius: 50%;
+      width: 148rpx;
+      height: 148rpx;
+      border: 2px solid #fff;
+    }
 
-      &__left {
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-      }
+    &__name {
+      color: #ffffff;
+      font-size: 35rpx;
+      margin-left: 38rpx;
+    }
 
-      &__icon {
-        width: 50rpx;
-        height: 51rpx;
-        margin-right: 20rpx;
-      }
-
-      &__name {
-        color: #000000;
-        font-size: 30rpx;
-      }
-
-      &__back {
-        width: 17rpx;
-        height: 30rpx;
-      }
+    &__address {
+      color: #ffffff;
+      font-size: 24rpx;
+      margin-top: 31rpx;
+      margin-left: 42rpx;
     }
   }
+
+  &__menu {
+    background-color: #ffffff;
+    margin-top: 26rpx;
+
+    &__item {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin: 0 30rpx;
+      padding: 40rpx 0;
+      border-bottom: 1rpx solid #f2f2f2;
+    }
+
+    &__left {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+    }
+
+    &__icon {
+      width: 42rpx;
+      height: 42rpx;
+      margin-right: 20rpx;
+    }
+
+    &__name {
+      color: #222;
+      font-size: 27rpx;
+    }
+
+    &__back {
+      width: 12rpx;
+      height: 22rpx;
+    }
+  }
+}
 </style>
